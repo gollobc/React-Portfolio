8806e3271ef6a8f4b6ceafe5c901bb9ab0ffcdfb
gollobc <cadegollob@gmail.com>
2023-09-08 20:08:58 -0700

    nav and footer

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
     <>
+      <nav>
+        <p>Projects</p>
+        <p>About</p>
+        <p>Contact</p>
+        <p>Resume</p>
+      </nav>
+
       <h1>Cade Gollob</h1>
+
+      <footer>
+        <a href='https://github.com/gollobc' target='_blank' rel="noreferrer noopener"><i className="fa-brands fa-github" ></i></a>
+        <a href='https://linkedin.com/in/cadegollob' target='_blank' rel="noreferrer noopener"><i className="fa-brands fa-linkedin" ></i></a>
+      </footer>
     </>
   )
 }
