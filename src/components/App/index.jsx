71d4c86c3951ceefa3cddfe2f265356c5eb84788
gollobc <cadegollob@gmail.com>
2023-09-12 23:54:30 -0700

    about

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -16,10 +16,12 @@ function App() {
 
       <div>
         <p>
+        I'm a performance and utility focused software engineer with a passion for health and fitness. Throughout my experience, I have developed a flexible mindset with the ability to see different aspects of a problem and explore alternative methods to construct an effective solution. My passion for software engineering is driven by my desire to help elevate people’s health and happiness.
 
+        I'm interested in developing machine learning skills. AI has incredible potential and I look forward to being a part of it.
         </p>
       </div>
-      
+
       <footer>
         <a href='https://github.com/gollobc' target='_blank' rel="noreferrer noopener"><i className="fa-brands fa-github" ></i></a>
         <a href='https://linkedin.com/in/cadegollob' target='_blank' rel="noreferrer noopener"><i className="fa-brands fa-linkedin" ></i></a>
